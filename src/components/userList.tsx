0acab65e11f8e52c38e3a7439f3bf68938128ca5
Almaz Kubakaev <almazkubakaev2607@gmail.com>
2023-05-05 19:51:14 +0600

    async ac + ts & hook useactions

--- src/components/userList.tsx
+++ src/components/userList.tsx
@@ -1,10 +1,30 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useTypedSelector } from "../hooks/useTypedSelector";
+import { useActions } from "../hooks/useActions";
 
 const UserList: React.FC = () => {
-  const {} = useTypedSelector((state) => state.user);
+  const { users, loading, error } = useTypedSelector((state) => state.user);
+  const { fetchUsers } = useActions();
 
-  return <div></div>;
+  useEffect(() => {
+    fetchUsers();
+  }, []);
+
+  if (loading) {
+    return <h3>loading...</h3>;
+  }
+
+  if (error) {
+    return <h3>{error}</h3>;
+  }
+
+  return (
+    <div>
+      {users.map((user) => (
+        <p key={user.id}>{user.name}</p>
+      ))}
+    </div>
+  );
 };
 
 export default UserList;
